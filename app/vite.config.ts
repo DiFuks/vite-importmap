cf4500479daa21a35511dea80cc6f2c0e088069b
Dmitry Fuks <difuks@gmail.com>
2022-11-28 13:49:48 +0300

    fix hmr

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,4 +1,4 @@
-import react, { Options } from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 import process from 'node:process'
 import path from 'path'
 import { defineConfig } from 'vite'
@@ -6,28 +6,19 @@ import { defineConfig } from 'vite'
 const rootPath = process.cwd()
 
 // eslint-disable-next-line import/no-default-export
-export default defineConfig(({ mode }) => {
-	const options: Options =
-		mode === `dev-single-spa`
-			? {
-					exclude: /.*/,
-			  }
-			: {}
-
-	return {
-		root: path.join(rootPath, `./src`),
-		plugins: [react(options)],
-		build: {
-			emptyOutDir: false,
-			rollupOptions: {
-				input: path.join(rootPath, `./src/singleSpa.tsx`),
-				preserveEntrySignatures: `strict`,
-				output: {
-					dir: path.join(rootPath, `./dist`),
-					entryFileNames: `singleSpa.js`,
-					format: `module`,
-				},
+export default defineConfig(() => ({
+	root: path.join(rootPath, `./src`),
+	plugins: [react()],
+	build: {
+		emptyOutDir: false,
+		rollupOptions: {
+			input: path.join(rootPath, `./src/singleSpa.tsx`),
+			preserveEntrySignatures: `strict`,
+			output: {
+				dir: path.join(rootPath, `./dist`),
+				entryFileNames: `singleSpa.js`,
+				format: `module`,
 			},
 		},
-	}
-})
+	},
+}))
