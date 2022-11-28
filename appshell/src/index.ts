78e56a6bb63f5537c43213d50ad30227ab4ffde3
Dmitry Fuks <difuks@gmail.com>
2022-11-28 12:56:43 +0300

    test dynamic imports in safari

--- appshell/src/index.ts
+++ appshell/src/index.ts
@@ -4,7 +4,7 @@ registerApplication(
 	`app`,
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
-	() => import(/* webpackIgnore: true */ `app`),
+	() => import(/* webpackIgnore: true */ `http://localhost:5173/singleSpa.tsx`),
 	location => location.pathname.startsWith(`/`),
 )
 
