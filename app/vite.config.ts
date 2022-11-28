a25f833ba803c5d71e65900085e85c0d7779430d
Dmitry Fuks <difuks@gmail.com>
2022-11-28 12:34:45 +0300

    fix "vite-plugin-react can't detect preamble"
    https://github.com/vitejs/vite/issues/1984

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,26 +1,33 @@
-import react from '@vitejs/plugin-react'
+import react, { Options } from '@vitejs/plugin-react'
 import process from 'node:process'
 import path from 'path'
-import { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
 
 const rootPath = process.cwd()
 
-const config: UserConfig = {
-	root: path.join(rootPath, `./src`),
-	plugins: [react()],
-	build: {
-		emptyOutDir: false,
-		rollupOptions: {
-			input: path.join(rootPath, `./src/singleSpa.tsx`),
-			preserveEntrySignatures: `strict`,
-			output: {
-				dir: path.join(rootPath, `./dist`),
-				entryFileNames: `singleSpa.js`,
-				format: `module`,
+// eslint-disable-next-line import/no-default-export
+export default defineConfig(({ mode }) => {
+	const options: Options =
+		mode === `dev-single-spa`
+			? {
+					exclude: /.*/,
+			  }
+			: {}
+
+	return {
+		root: path.join(rootPath, `./src`),
+		plugins: [react(options)],
+		build: {
+			emptyOutDir: false,
+			rollupOptions: {
+				input: path.join(rootPath, `./src/singleSpa.tsx`),
+				preserveEntrySignatures: `strict`,
+				output: {
+					dir: path.join(rootPath, `./dist`),
+					entryFileNames: `singleSpa.js`,
+					format: `module`,
+				},
 			},
 		},
-	},
-}
-
-// eslint-disable-next-line import/no-default-export
-export default config
+	}
+})
